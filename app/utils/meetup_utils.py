35a02fd91485c9da667896e313413caf7b96a818
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2016-06-09 00:09:30 -0700

    - added test to verify the latam filter only brings groups that belong to latam countries  - added test to verify the colombia filter only brings colombian groups countries  - deleted genereated files content from readme.txt

--- app/utils/meetup_utils.py
+++ app/utils/meetup_utils.py
@@ -3,21 +3,18 @@ import meetup.api
 
 class MeetupUtils:
 
-    def __init__(self, apy_key):
-        # TODO: change api
-        self.__client__ = meetup.api.Client(apy_key)
+    def __init__(self, api_key):
+        self.__client__ = meetup.api.Client(api_key)
 
     def get_find_groups(self, places_filters, keyword_filters):
         found_places = []
         for place in places_filters:
             find_group_filter = {'country': place['country'], 'location': place['location'], 'text': 'python',
                                  "distance": 100}
-            # print("find_group_filter", find_group_filter)
             python_groups = self.__client__.GetFindGroups(find_group_filter)
             for python_group in python_groups:
-                print("python_group", str(python_group.__dict__))
                 for keyword_filter in keyword_filters:
-                    # if the keyword is in the group name, the group is added to the list
+                    # if the keyword is in the group name, the group is added to the list to be returned
                     if keyword_filter in python_group.name:
                         found_places.append({
                             'country_code': python_group.country,
--- test/test_python_groups.py
+++ test/test_python_groups.py
@@ -1,5 +1,5 @@
 import unittest
-from app.python_groups import process_python_group_by_location, get_python_groups
+from app.python_groups import process_python_group_by_location, get_location_based_python_groups
 
 
 class TestPythonGroups(unittest.TestCase):
@@ -23,29 +23,19 @@ class TestPythonGroups(unittest.TestCase):
         ]
         self.__latam_country_codes__.append(self.__colombia_code__)
 
-    def test_get_all_python_groups(self):
-
-        # process colombia info from meetup api
-        process_python_group_by_location("CO")
-
-        # process latam info from meetup api
-        process_python_group_by_location("LATAM")
-
-    def test_get_location_based_python_groups(self):
+    def test_get_colombia_based_python_groups(self):
 
         # test retrieved group from colombia
-        colombian_groups = get_python_groups("CO")
+        colombian_groups = get_location_based_python_groups("CO")
         for colombian_group in colombian_groups:
             self.assertEqual(colombian_group['country_code'], self.__colombia_code__)
 
-        # test retrieved group from colombia
-        latam_groups = get_python_groups("LATAM")
+    def test_get_latam_based_python_groups(self):
+
+        # test retrieved group from latam
+        latam_groups = get_location_based_python_groups("LATAM")
         for latam_group in latam_groups:
             self.assertTrue(latam_group['country_code'] in self.__latam_country_codes__)
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
